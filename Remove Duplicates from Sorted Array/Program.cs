775747abe0729ebb645c7f7fa6c3e4fdc8c17fd8
SvetlozarStoyanov <svetlio214@gmail.com>
2024-03-19 15:24:11 +0200

    Added better solution

--- Remove Duplicates from Sorted Array/Program.cs
+++ Remove Duplicates from Sorted Array/Program.cs
@@ -4,40 +4,32 @@
     {
         static void Main(string[] args)
         {
-            //var firstTestArray = new int[] { 1, 1, 2 };
-            //Console.WriteLine(RemoveDuplicates(firstTestArray));
+            var firstTestArray = new int[] { 1, 1, 2 };
+            Console.WriteLine(RemoveDuplicates(firstTestArray));
             //var secondTestArray = new int[] { 1, 1, 2, 3, 4 };
             //Console.WriteLine(RemoveDuplicates(secondTestArray));            
             //var thirdTestArray = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
             //Console.WriteLine(RemoveDuplicates(thirdTestArray));
             //var fourthTestArray = new int[] { 1, 1 };
-            //Console.WriteLine(RemoveDuplicates(fourthTestArray));            
-            var fifthTestArray = new int[] { 1, 2 };
-            Console.WriteLine(RemoveDuplicates(fifthTestArray));
+            //Console.WriteLine(RemoveDuplicates(fourthTestArray));
+            //var fifthTestArray = new int[] { 1, 2 };
+            //Console.WriteLine(RemoveDuplicates(fifthTestArray));
         }
 
         public static int RemoveDuplicates(int[] nums)
         {
-            var list = new List<int>();
-            var repeating = 0;
-            for (int i = 0; i < nums.Length; i++)
+            var reachedIndex = 0;
+            for (int i = 1; i < nums.Length; i++)
             {
-                if (list.Contains(nums[i]))
+                if (nums[i] > nums[reachedIndex])
                 {
-                    repeating++;
-                }
-                else
-                {
-                    list.Add(nums[i]);
+                    reachedIndex++;
+                    nums[reachedIndex] = nums[i];
                 }
             }
-            for (int i = 0; i < list.Count; i++)
-            {
-                nums[i] = list[i];
-            }
-            return nums.Length - repeating;
+
+            return reachedIndex + 1;
         }
 
-        
     }
 }
