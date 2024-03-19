43eb30cf23d997b07a5db965531107da17cf5bfe
SvetlozarStoyanov <svetlio214@gmail.com>
2024-03-19 18:27:24 +0200

    Added Solutions for Contains Duplicate 1 and 2

---
+++ Contains Duplicate II/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Contains_Duplicate_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTestArray = new int[] { 1, 2, 3, 1 };
+            Console.WriteLine(ContainsNearbyDuplicate(firstTestArray, 3));
+            var secondTestArray = new int[] { 1, 2, 3, 1, 2, 3 };
+            Console.WriteLine(ContainsNearbyDuplicate(secondTestArray, 2));
+        }
+
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dictionary.ContainsKey(nums[i]) && Math.Abs(dictionary[nums[i]] - i) <= k)
+                {
+                    return true;
+                }
+                dictionary[nums[i]] = i;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Contains Duplicate/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Contains_Duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTestArray = new int[] { 1, 2, 3, 1 };
+            Console.WriteLine(ContainsDuplicate(firstTestArray));
+        }
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            //var distinctCount = nums.Distinct().Count();
+            //if (distinctCount != nums.Length)
+            //{
+            //    return true;
+            //}
+            var hashset = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashset.Contains(nums[i]))
+                {
+                    return true;
+                }
+                hashset.Add(nums[i]);
+            }
+
+            return false;
+        }
+    }
+}
