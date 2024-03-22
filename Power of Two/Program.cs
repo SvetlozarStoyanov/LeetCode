0fb52ac74a58d34332d0057d76e4166790164262
SvetlozarStoyanov <svetlio214@gmail.com>
2024-03-22 11:28:28 +0200

    Added solution of Power of Two problem

---
+++ Power of Two/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Power_of_Two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTestNumber = 16;
+            Console.WriteLine(IsPowerOfTwo(firstTestNumber));
+        }
+
+        public static bool IsPowerOfTwo(int n)
+        {
+            
+            if (n <= 0)
+            {
+                return false;
+            }
+            while (n > 1)
+            {
+                if (n % 2 != 0)
+                {
+                    return false;
+                }
+                n /= 2;
+            }
+
+            return true;
+        }
+    }
+}
