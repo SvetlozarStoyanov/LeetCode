6165dfc105029a5b5d8a31393765fe389c7ea5f0
SvetlozarStoyanov <svetlio214@gmail.com>
2024-03-23 12:04:26 +0200

    Added solution for Implementing queue using stacks

---
+++ Implement Stack using Queues/MyStack.cs
@@ -0,0 +1,57 @@
+﻿namespace Implement_Stack_using_Queues
+{
+    public class MyStack
+    {
+        private Queue<int> queue;
+        public MyStack()
+        {
+            queue = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            queue.Enqueue(x);
+        }
+
+        public int Pop()
+        {
+            var tempQueue = new Queue<int>();
+            var removedElement = 0;
+            while (queue.Count > 0)
+            {
+                removedElement = queue.Dequeue();
+                if (queue.Count > 0)
+                {
+                    tempQueue.Enqueue(removedElement);
+                }
+
+            }
+            queue = tempQueue;
+            return removedElement;
+        }
+
+        public int Top()
+        {
+            var tempQueue = new Queue<int>();
+            var topElement = 0;
+            while (queue.Count > 0)
+            {
+                var removedElement = queue.Dequeue();
+                tempQueue.Enqueue(removedElement);
+
+                if (queue.Count == 0)
+                {
+                    topElement = removedElement;
+                }
+
+            }
+            queue = tempQueue;
+            return topElement;
+        }
+
+        public bool Empty()
+        {
+            return queue.Count == 0;
+        }
+    }
+}
---
+++ Implement Stack using Queues/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Implement_Stack_using_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStack = new MyStack();
+            myStack.Push(1);
+            myStack.Push(2);
+            Console.WriteLine(myStack.Top());
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Empty());
+            Console.WriteLine();
+        }
+
+
+    }
+}
