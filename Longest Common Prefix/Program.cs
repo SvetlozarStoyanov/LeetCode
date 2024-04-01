7ea23b65c5d65ef1951c6ba514132d0fc8a861d3
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-01 19:42:19 +0300

    Added solution for Longest Common Prefix problem

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Longest_Common_Prefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTestArray = new string[] { "flower", "flow", "flight" };
+            Console.WriteLine(LongestCommonPrefix(firstTestArray));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var lengthOfLongestPrefix = 0;
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+
+                if (strs.All(s => s.Length > lengthOfLongestPrefix
+                && s[lengthOfLongestPrefix] == strs[0][lengthOfLongestPrefix]))
+                {
+                    lengthOfLongestPrefix++;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            return strs[0].Substring(0, lengthOfLongestPrefix);
+        }
+    }
+}
