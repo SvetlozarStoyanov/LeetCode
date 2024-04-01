4be6a37a4e3c85a932102d231afdaf5363690edd
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-01 18:46:28 +0300

    Added solution for Length of last word problem

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Length_of_Last_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            var arraySplit = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return arraySplit[arraySplit.Length - 1].Length;
+        }
+    }
+}
