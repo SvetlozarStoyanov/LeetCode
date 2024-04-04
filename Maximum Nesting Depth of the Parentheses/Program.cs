2a9e0b266dcfe26ae2c0fcd6006209f8be10963b
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-04 17:57:16 +0300

    Added solution to Maxium Nesting Depth of Parentheses problem

---
+++ Maximum Nesting Depth of the Parentheses/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Maximum_Nesting_Depth_of_the_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTestString = "(1+(2*3)+((8)/4))+1";
+            Console.WriteLine(MaxDepth(firstTestString));
+            var secondTestString = "";
+            Console.WriteLine(MaxDepth(secondTestString));
+
+        }
+
+        public static int MaxDepth(string s)
+        {
+            var openBrackets = 0;
+            var maxCount = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    openBrackets++;
+                }
+                else if (s[i] == ')')
+                {
+                    if (openBrackets > maxCount)
+                    {
+                        maxCount = openBrackets;
+                    }
+                    openBrackets--;
+                }
+            }
+            return maxCount;
+        }
+    }
+}
