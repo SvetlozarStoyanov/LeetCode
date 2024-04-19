b3e4945e7522923644f42b264b2f82b5e200d543
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-19 10:22:02 +0300

    Added solution for Squares of Sorted Array

---
+++ Squares of a Sorted Array/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Squares_of_a_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var firstTestArray = new[] { -4, -1, 0, 3, 10 };
+            //Console.WriteLine(string.Join(", ", SortedSquares(firstTestArray)));            
+            var secondTestArray = new[] { -7, -3, 2, 3, 11 };
+            Console.WriteLine(string.Join(", ", SortedSquares(secondTestArray)));
+        }
+
+        public static int[] SortedSquares(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            var result = new int[nums.Length];
+            var reachedIndex = nums.Length - 1;
+            while (left <= right)
+            {
+                if (nums[left] * nums[left] > nums[right] * nums[right])
+                {
+                    result[reachedIndex] = nums[left] * nums[left];
+                    left++;
+                }
+                else
+                {
+                    result[reachedIndex] = nums[right] * nums[right];
+                    right--;
+                }
+                reachedIndex--;
+            }
+
+            return result;
+        }
+    }
+}
