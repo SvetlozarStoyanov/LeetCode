a6b02a5314bd572e11503518452de6eab3362ee4
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-24 09:45:19 +0300

    Added solution for Fibonacci Number problem

---
+++ Fibonacci Number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Fibonacci_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(4));
+        }
+
+        public static int Fib(int n)
+        {
+            if (n < 1)
+            {
+                return 0;
+            }
+            var first = 0;
+            var second = 1;
+            var result = first + second;
+            for (int i = 1; i < n; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            return result;
+        }
+    }
+}
