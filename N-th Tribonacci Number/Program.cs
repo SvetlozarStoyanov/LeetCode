5299ee653a6871dd7f97332bd6ee5b6f232843a0
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-24 09:45:01 +0300

    Added solution for N-th Tribonacci Number problem

---
+++ N-th Tribonacci Number/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace N_th_Tribonacci_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Tribonacci(4));
+        }
+
+        public static int Tribonacci(int n)
+        {
+            var first = 0;
+            var second = 1;
+            var third = 1;
+            var curr = 0;
+            if (n == 1)
+            {
+                return first;
+            }
+            if (n == 2)
+            {
+                return second;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                curr = first + second + third;
+                first = second;
+                second = third;
+                third = curr;
+            }
+            return curr;
+        }
+    }
+}
