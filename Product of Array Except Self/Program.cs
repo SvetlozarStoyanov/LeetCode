5c522cb49065cad1d645265e2d67faf59a56b462
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-29 09:38:35 +0300

    Added solution for Product of Array Except Self problem

---
+++ Product of Array Except Self/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Product_of_Array_Except_Self
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTestArray = new int[] { 1, 2, 3, 4 };
+            Console.WriteLine(string.Join(",", ProductExceptSelf(firstTestArray)));
+            var secondTestArray = new int[] { -1, 1, 0, -3, 3 };
+            Console.WriteLine(string.Join(",", ProductExceptSelf(secondTestArray)));
+        }
+
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            var resultArray = new int[nums.Length];
+            var numbersAndProducts = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numbersAndProducts.ContainsKey(nums[i]))
+                {
+                    resultArray[i] = numbersAndProducts[nums[i]];
+                    continue;
+                }
+                var skipIndex = i;
+                var currIndex = 0;
+                var product = 1;
+                while (currIndex < nums.Length)
+                {
+                    if (currIndex != skipIndex)
+                    {
+                        if (nums[currIndex] == 0)
+                        {
+                            product = 0;
+                            break;
+                        }
+                        product *= nums[currIndex];
+                    }
+                    currIndex++;
+                }
+
+                numbersAndProducts[nums[i]] = product;
+                resultArray[i] = product;
+            }
+            return resultArray;
+        }
+    }
+}
