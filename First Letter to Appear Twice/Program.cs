a8f6aa40009fdfa79e71b337d08abc6522b1535e
SvetlozarStoyanov <svetlio214@gmail.com>
2024-05-06 16:12:54 +0300

    Added solution for First Letter to Appear Twice

---
+++ First Letter to Appear Twice/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace First_Letter_to_Appear_Twice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static char RepeatedCharacter(string s)
+        {
+            var letterList = new List<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (letterList.Contains(s[i]))
+                {
+                    return s[i];
+                }
+                letterList.Add(s[i]);
+            }
+
+            return s[0];
+        }
+    }
+}
