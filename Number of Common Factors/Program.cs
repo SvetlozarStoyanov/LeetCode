65d52af592da0a473aaddbdbce40877314180465
SvetlozarStoyanov <svetlio214@gmail.com>
2024-05-14 17:42:35 +0300

    Added solution for Number of Common factors problem

---
+++ Number of Common Factors/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Number_of_Common_Factors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CommonFactors(390, 780));
+        }
+
+        public static int CommonFactors(int a, int b)
+        {
+            var min = Math.Min(a, b);
+            var currFactor = 1;
+            var factorCount = 0;
+            while (currFactor <= min)
+            {
+                if (a % currFactor == 0 && b % currFactor == 0)
+                {
+                    factorCount++;
+                }
+                currFactor++;
+            }
+            return factorCount;
+        }
+    }
+}
