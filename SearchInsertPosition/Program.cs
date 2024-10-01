c31c3eecddda7a804f97ddfa94f93eca38b4a356
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-01 21:39:56 +0300

    Added solution for Search insert Position Problem

---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SearchInsertPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 5, 6 };
+            Console.WriteLine(SearchInsert(nums, 7));
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+
+            while (left < right)
+            {
+                if (nums[left] < target)
+                {
+                    left++;
+                }
+                if (nums[right] > target)
+                {
+                    right--;
+                }
+                if (nums[left] == target)
+                {
+                    return left;
+                }
+                if (nums[right] == target)
+                {
+                    return right;
+                }
+            }
+
+            if (nums[left] < target)
+            {
+                left++;
+            }
+
+            return left;
+        }
+    }
+}
