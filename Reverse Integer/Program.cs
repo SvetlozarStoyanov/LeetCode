4421dfc8bbd47950674f73029742b23bea5caa0f
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-21 17:36:49 +0300

    Added solution for Reverse Integer problem

---
+++ Reverse Integer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Reverse_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(-123));
+        }
+
+        public static int Reverse(int x)
+        {
+            var sb = new StringBuilder();
+
+            var isNegative = false;
+            if (x < 0)
+            {
+                isNegative = true;
+                x *= -1;
+            }
+
+            while (x != 0)
+            {
+                var digit = x % 10;
+                x /= 10;
+                sb.Append(digit);
+            }
+
+            if (int.TryParse(string.Join("", sb), out int resultNum))
+            {
+                if (isNegative)
+                {
+                    resultNum *= -1;
+                }
+                return resultNum;
+            }
+            return 0;
+        }
+    }
+}
