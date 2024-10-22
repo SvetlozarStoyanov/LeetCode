fc9086b7f5369e6325932d5e04e0b5bbb0eaeec7
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-22 16:22:35 +0300

    Added solution for divide two integers

---
+++ Divide Two Integers/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Divide_Two_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(-1 << 1);
+            Console.WriteLine(Divide(10, 1));
+        }
+
+        public static int Divide(int dividend, int divisor)
+        {
+            var result = 0;
+            if (dividend == int.MinValue && divisor == 1)
+            {
+                return dividend;
+            }
+            else if (dividend == int.MinValue && divisor == -1)
+            {
+                return int.MaxValue;
+            }            
+            if (dividend == int.MaxValue && divisor == 1)
+            {
+                return dividend;
+            }
+            else if (dividend == int.MaxValue && divisor == -1)
+            {
+                return -int.MaxValue;
+            }
+
+            var tempDividend = Math.Abs(dividend);
+            var tempDivisor = Math.Abs(divisor);
+
+            while (tempDividend >= tempDivisor)
+            {
+                var powerOfTwo = 0;
+                while (tempDividend >= tempDivisor << 1 << powerOfTwo)
+                {
+                    powerOfTwo++;
+                }
+                result += 1 << powerOfTwo;
+                tempDividend -= tempDivisor << powerOfTwo;
+            }
+
+            if (Math.Sign(dividend) != Math.Sign(divisor))
+            {
+                return -result;
+            }
+
+            return result;
+        }
+    }
+}
