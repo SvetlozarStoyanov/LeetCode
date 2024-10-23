689cd730e82137d929655f54b5376ca8fc2d4eb4
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-23 19:00:13 +0300

    Added Solution for Pow(x, n) problem

---
+++ Pow(x, n)/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Pow_x__n_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyPow(-10, 0));
+        }
+
+        public static double MyPow(double x, int n)
+        {
+            var result = 1d;
+            if (Math.Abs(x) == 1)
+            {
+                return x;
+            }
+            if (n > 0)
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+                    result *= x;
+                }
+            }
+            else if (n < 0)
+            {
+                var power = Math.Abs((long)n);
+                for (int i = 0; i < power; i++)
+                {
+                    result /= x;
+                    if (result == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = Math.Sign(x) * x / x;
+            }
+            return result;
+        }
+    }
+}
