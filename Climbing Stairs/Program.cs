0f20bce3aff2e51186403ef2c713583e6e4f2a26
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-26 14:44:41 +0300

    Added solution for Climbing Stairs problem

---
+++ Climbing Stairs/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Climbing_Stairs
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClimbStairs(2));
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            var left = 1;
+            var right = 2;
+            if (n == 1)
+            {
+                return left;
+            }
+            else if (n == 2)
+            {
+                return right;
+            }
+
+            var curr = left + right;
+            var index = 2;
+
+            while (index < n)
+            {
+                curr = left + right;
+                left = right;
+                right = curr;
+                index++;
+            }
+
+            return curr;
+        }
+    }
+}
