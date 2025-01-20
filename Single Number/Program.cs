f9b4d3b236c2b782de9a6308947913e9f9672fdf
SvetlozarStoyanov <svetlio214@gmail.com>
2025-01-20 12:39:30 +0200

    Added alternate solution to single number problem

--- Single Number/Program.cs
+++ Single Number/Program.cs
@@ -27,90 +27,16 @@
 
         public static int SingleNumberAlternate(int[] nums)
         {
-            if (nums.Length == 1)
+            var hashset = new HashSet<int>();
+            foreach (var number in nums)
             {
-                return nums[0];
-            }
-
-            MergeSort(nums);
-
-            for (int i = 0; i < nums.Length - 1; i += 2)
-            {
-                if (nums[i] != nums[i + 1])
+                if (!hashset.Remove(number))
                 {
-                    return nums[i];
+                    hashset.Add(number);
                 }
             }
 
-            return nums[nums.Length - 1];
-        }
-
-        private static void MergeSort(int[] array)
-        {
-            if (array.Length < 2)
-            {
-                return;
-            }
-            var splitArrays = SplitIntoTwoArrays(array);
-            MergeSort(splitArrays.Item1);
-            MergeSort(splitArrays.Item2);
-            Merge(splitArrays.Item1, splitArrays.Item2, array);
-        }
-
-        private static void Merge(int[] arrayOne, int[] arrayTwo, int[] resultArray)
-        {
-            var arrayOneIndex = 0;
-            var arrayTwoIndex = 0;
-            var resultArrayIndex = 0;
-
-
-            while (arrayOneIndex < arrayOne.Length && arrayTwoIndex < arrayTwo.Length)
-            {
-                if (arrayOne[arrayOneIndex] < arrayTwo[arrayTwoIndex])
-                {
-                    resultArray[resultArrayIndex++] = arrayOne[arrayOneIndex++];
-                }
-                else
-                {
-                    resultArray[resultArrayIndex++] = arrayTwo[arrayTwoIndex++];
-                }
-            }
-
-            while (arrayOneIndex < arrayOne.Length)
-            {
-                resultArray[resultArrayIndex++] = arrayOne[arrayOneIndex++];
-            }
-
-            while (arrayTwoIndex < arrayTwo.Length)
-            {
-                resultArray[resultArrayIndex++] = arrayTwo[arrayTwoIndex++];
-            }
-        }
-
-        private static ValueTuple<int[], int[]> SplitIntoTwoArrays(int[] array)
-        {
-            var result = new ValueTuple<int[], int[]>();
-
-            var middle = array.Length / 2;
-            var arrayOne = new int[middle];
-
-            for (int i = 0; i < middle; i++)
-            {
-                arrayOne[i] = array[i];
-            }
-
-            result.Item1 = arrayOne;
-
-            var arrayTwo = new int[array.Length - middle];
-
-            for (int i = 0; i < arrayTwo.Length; i++)
-            {
-                arrayTwo[i] = array[middle + i];
-            }
-
-            result.Item2 = arrayTwo;
-
-            return result;
+            return hashset.FirstOrDefault();
         }
     }
 }
