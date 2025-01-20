136596ac1f259b93b4150668a0110f09ce98f243
SvetlozarStoyanov <svetlio214@gmail.com>
2025-01-20 12:38:51 +0200

    Added solution to single number 2 problem

---
+++ Single Number II/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Single_Number_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            foreach (int number in nums)
+            {
+                if (!dictionary.ContainsKey(number))
+                {
+                    dictionary.Add(number, 0);
+                }
+                dictionary[number]++;
+            }
+
+            return dictionary.FirstOrDefault(x => x.Value == 1).Key;
+        }
+    }
+}
