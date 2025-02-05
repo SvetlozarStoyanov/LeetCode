de551dbf721ec3796b7e3a64e91a91116b1be3d2
SvetlozarStoyanov <svetlio214@gmail.com>
2025-02-05 15:56:03 +0200

    Added solution to Linked List cycle problem

---
+++ Linked List Cycle/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Linked_List_Cycle
+{
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static bool HasCycle(ListNode head)
+        {
+            var curr = head;
+            var visited = new HashSet<ListNode>();
+            while (curr != null)
+            {
+                if (visited.Contains(curr))
+                {
+                    return true;
+                }
+                visited.Add(curr);
+                curr = curr.next;
+            }
+            return false;
+        }
+    }
+}
