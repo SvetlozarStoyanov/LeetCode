07ae81d245b7d01ef140f1a67bd8b4e41436ed59
SvetlozarStoyanov <svetlio214@gmail.com>
2025-03-26 10:16:09 +0200

    Added solution for Binary Tree Preorder Traversal problem

---
+++ Binary Tree Preorder Traversal/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Binary_Tree_Preorder_Traversal
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private static IList<int> preorderTraversalList;
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            preorderTraversalList = new List<int>();
+            DFS(root);
+            return preorderTraversalList;
+        }
+
+        private static void DFS(TreeNode node)
+        {
+            if (node is null)
+            {
+                return;
+            }
+
+            preorderTraversalList.Add(node.val);
+
+            DFS(node.left);
+            DFS(node.right);
+        }
+    }
+}
