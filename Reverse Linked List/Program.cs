7964104eacd43d8bf18ea9f95fc3dd02e6c8c74d
Svetlozar Stoyanov <svetlio214@gmail.com>
2025-09-03 17:10:35 +0300

    Added Solution for reverse linked list problem

---
+++ Reverse Linked List/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Reverse_Linked_List
+{
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x, ListNode next = null)
+            {
+                val = x;
+                this.next = next;
+            }
+        }
+
+        private static ListNode tail;
+        static void Main(string[] args)
+        {
+            //ReverseList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
+            ReverseList(new ListNode(1, new ListNode(2)));
+        }
+
+        private static ListNode ReverseList(ListNode head)
+        {
+            if (head is null)
+            {
+                return null;
+            }
+            if (head.next is null)
+            {
+                return head;
+            }
+            tail = head;
+
+            Recursion(head, head.next);
+            head.next = null;
+
+            return tail;
+        }
+
+        private static void Recursion(ListNode prev, ListNode curr)
+        {
+            if (curr.next is null)
+            {
+                tail = curr;
+                tail.next = prev;
+                return;
+            }
+
+            Recursion(curr, curr.next);
+
+            curr.next = prev;
+        }
+    }
+}
