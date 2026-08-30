732aacb17c2d6715e3c18e34fb68d9172e188f0e
Svetlozar Stoyanov <svetlio214@gmail.com>
2026-08-30 18:49:37 +0300

    Added solution for Summary Ranges problem

---
+++ First Missing Positive/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace First_Missing_Positive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstMissingPositive([1, 2, 0]));
+        }
+
+        public static int FirstMissingPositive(int[] nums)
+        {
+            var foundPositives = new HashSet<int>(1);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    foundPositives.Add(nums[i]);
+                }
+            }
+
+            if (foundPositives.Count == 0 )
+            {
+                return 1;
+            }
+
+            foundPositives = foundPositives.OrderBy(x => x).ToHashSet();
+
+            if (foundPositives.First() == 1)
+            {
+                return 1;
+            }
+
+            foreach (var item in foundPositives)
+            {
+                if (!foundPositives.Contains(item + 1))
+                {
+                    return item + 1;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ Summary Ranges/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Summary_Ranges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(string.Join("\n", SummaryRanges([0, 2, 3, 4, 6, 8, 9])));
+            //Console.WriteLine(string.Join("\n", SummaryRanges([0, 1, 2, 4, 5, 7])));
+            Console.WriteLine(string.Join("\n", SummaryRanges([-2147483648, 0, 2, 3, 4, 6, 8, 9])));
+        }
+
+        public static IList<string> SummaryRanges(int[] nums)
+        {
+            var ranges = new List<string>();
+
+            if (nums.Length == 0)
+            {
+                return ranges;
+            }
+            if (nums.Length == 1)
+            {
+                ranges.Add($"{nums[0]}");
+                return ranges;
+            }
+
+            var index = 1;
+            var curr = nums[index];
+            var prev = nums[index - 1];
+            var rangeStart = prev;
+            var lastAddedIndex = -1;
+            while (index < nums.Length)
+            {
+                curr = nums[index];
+                prev = nums[index - 1];
+                if (Math.Abs((long)curr - prev) > 1)
+                {
+                    if (rangeStart != prev)
+                    {
+                        ranges.Add($"{rangeStart}->{prev}");
+                    }
+                    else
+                    {
+                        ranges.Add($"{prev}");
+                    }
+                    rangeStart = curr;
+                    lastAddedIndex = index - 1;
+                }
+                
+                index++;
+            }
+            if (lastAddedIndex != index)
+            {
+                if (rangeStart != curr)
+                {
+                    ranges.Add($"{rangeStart}->{curr}");
+                }
+                else
+                {
+                    ranges.Add($"{curr}");
+                }
+            }
+            return ranges;
+        }
+    }
+}
