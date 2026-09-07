e962e1ecbdd1a4fa4e79e3d81ab4da768f2f7e81
Svetlozar Stoyanov <svetlio214@gmail.com>
2026-09-07 21:16:40 +0300

    Added solution for Valid Anagram problem

---
+++ Valid Anagram/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Valid_Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("anagram", "nagaram"));
+            //Console.WriteLine(IsAnagram("ab", "a"));
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+            var dictionary = new Dictionary<char, int>();
+
+            foreach (var item in s)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    dictionary[item] = 0;
+                }
+                dictionary[item]++;
+            }
+
+            foreach (var item in t)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    return false;
+                }
+
+                dictionary[item]--;
+                if (dictionary[item] == 0)
+                {
+                    dictionary.Remove(item);
+                }
+            }
+            
+            return !dictionary.Any();
+        }
+    }
+}
